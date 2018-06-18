547d8afb17759be4a6678cc47e0e2a06099388d8
johden2 <johden2@163.com>
2018-06-18 22:57:06 +0800

    修改导出功能

---
+++ MES/MESSolution/MES.ManagementApp/Controllers/Common/SysExportHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.EnterpriseServices;
+using System.Web.Script.Serialization;
+using Sys.Model;
+using Sys.Dao;
+using CellDLL = Aspose.Cells;
+using Sys.Tools;
+using Aspose.Words;
+using Aspose.Cells.Drawing;
+using Sys.Model;
+using System.Data;
+using Aspose.Words.Drawing;
+using System.Reflection;
+
+namespace MES.ManagementApp.Controllers
+{
+    /// <summary>
+    /// 系统导出功能
+    /// </summary>
+    public partial class SysExportHelper
+    {
+        #region Begin 通用导出
+
+        /// <summary>
+        /// 通用导出
+        /// </summary>
+        /// <param name="businessObj"></param>
+        public static string Export<T>(ref BussinessModel businessObj, List<T> list) where T : class
+        {
+            string message = string.Empty;
+            try
+            {
+                //1.生成文件(由模板生成新的文件)
+                string templateName = "ExportTemplate.xls";
+                string templatePath = HttpContext.Current.Server.MapPath("~" + SysConfigHelper.TemplateFolder) + templateName;
+                string fileName = string.Format("{0}_{1}.xls", businessObj.BusinessType, DateTime.Now.ToString("yyMMddHHmmssffff"));
+                businessObj.FilePath = businessObj.TempPath + fileName;
+                businessObj.FileName = fileName;
+                message = FileHelper.CopyFile(templatePath, businessObj.TempPath.Trim('/'), fileName);
+                if (!string.IsNullOrEmpty(message)) return message;
+
+                //2.填写文件内容
+                CellDLL.Workbook workbook = new CellDLL.Workbook(businessObj.FilePath);
+                CellDLL.Cells cells = workbook.Worksheets[0].Cells;//单元格 
+                int index = 0;
+                int colNum = 0;
+                object value = null;
+                IDictionary<string,PropertyInfo> colProperties = new Dictionary<string,PropertyInfo>();
+                PropertyInfo property = null;
+                Type type = typeof(T);
+                if (businessObj.ColList != null && businessObj.ColList.Count > 0)
+                {
+                    //(1)先写列头
+                    businessObj.ColList.ForEach(colName =>
+                    {
+                        cells[index, colNum++].Value = colName;
+                        if (!colProperties.ContainsKey(colName))
+                        {
+                            property = type.GetProperty(colName);
+                            colProperties.Add(colName, property);
+                        }
+                    });
+
+                    //(2)写内容
+                    if (list != null && list.Count > 0)
+                    {
+                        index = 1;
+                        colNum = 0;
+                        foreach (var item in list)
+                        {
+                            foreach (string colName in businessObj.ColList)
+                            {
+                                property = colProperties[colName];
+                                if (property != null)
+                                {
+                                    value = property.GetValue(item, null);
+                                    cells[index, colNum].Value = TConvertHelper.FormatDBString(value);
+                                }
+
+                                colNum++;
+                            }
+                            colNum = 0;
+                            index++;
+                        }
+                    }
+
+                }/*businessObj.ColList*/
+
+                //3.保存文件
+                workbook.Save(businessObj.FilePath);
+            }
+            catch (Exception ex)
+            {
+                return "导出文件时出错,文件可能正被打开！";
+            }
+            finally
+            {
+                //xlApp.Quit();
+                GC.Collect();//强行销毁  
+            }
+
+            return string.Empty;
+        }
+    
+        #endregion
+
+    }
+}
---
+++ MES/MESSolution/Sys/Model/Custom/BussinessModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace Sys.Model
+{
+    /// <summary>
+    /// 业务模型
+    /// </summary>
+    public class BussinessModel
+    {
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public string BusinessType { get; set; }
+
+        /// <summary>
+        /// 关键参数值
+        /// </summary>
+        public string KeyValue { get; set; }
+
+        /// <summary>
+        /// 临时文件路径
+        /// </summary>
+        public string TempPath { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 用于导出时控制需要显示的列
+        /// </summary>
+        public List<string> ColList { get; set; }
+
+    }
+}
